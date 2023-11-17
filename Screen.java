88058348561ab2fa03e03d1a8ede9792d861ed27
Beck <rmarshall26@amherst.edu>
2023-11-17 17:41:49 -0500

    Reverted to 11/17 at 3:55

--- Screen.java
+++ Screen.java
@@ -4,96 +4,91 @@ import java.awt.event.*;
 
 
 public class Screen {
-   static int boxSize = 64;
-   Balloons balloon;
-    static boolean titleScreen = true;
-    static boolean chooseMapScreen = false;
-    static boolean map1Screen = false;
-    static boolean map2Screen = false;
-    static boolean map3Screen = false;
-
-   //This method draws the Title Screen.
-   public void drawTitleScreen(Graphics g, int width, int height){
-       Button DrawStart = TowerDefense.arrayOfButtons.Start;
-       g.setColor(Color.BLACK);
-       g.fillRect(0,0,width,height);
-       g.setColor(Color.white);
-       g.drawString("Amherst Tower Defense",width/2-70, height/2);
-       g.drawRect((int) DrawStart.x,(int)DrawStart.y,(int)DrawStart.width,(int)DrawStart.height);
-       g.drawString("Start",width/2-10,height*5/8);
-       g.setColor(Color.pink);
-
-   }
+    static int boxSize = 64;
+    Balloons balloon;
 
 
-   //This method draws the screen where the user can choose their map.
-   public void drawChooseMapScreen(Graphics g,int width, int height){
-       g.setColor(Color.BLACK);
-       g.fillRect(0,0,width,height);
-       g.setColor(Color.green);
-       drawMap1Screen(g,boxSize*3/2,boxSize*2,boxSize*4,boxSize*3,16);
-       g.setColor(Color.cyan);
-       drawMap2Screen(g,boxSize*13/2,boxSize*2,boxSize*4,boxSize*3,16);
-       g.setColor(Color.pink);
-       drawMap3Screen(g,boxSize*23/2,boxSize*2,boxSize*4,boxSize*3,16);
-       g.setColor(Color.WHITE);
-       g.drawString("Choose Your Map:",width/2-70,50);
-       g.drawRect(boxSize,boxSize*2,boxSize*4,boxSize*3);
-       g.drawRect(boxSize*6,boxSize*2,boxSize*4,boxSize*3);
-       g.drawRect(boxSize*11,boxSize*2,boxSize*4,boxSize*3);
-   }
+    //This method draws the Title Screen.
+    public void drawTitleScreen(Graphics g, int width, int height){
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,width,height);
+        g.setColor(Color.white);
+        g.drawString("Amherst Tower Defense",width/2-70, height/2);
+        g.drawRect((int)TowerDefense.Start.x,(int)TowerDefense.Start.y,(int)TowerDefense.Start.width,(int)TowerDefense.Start.height);
+        g.drawString("Start",width/2-10,height*5/8);
+        g.setColor(Color.pink);
+
+    }
 
 
-   //This method draws the screen for the first map.
-   public void drawMap1Screen(Graphics g,int x, int y,int width, int height,int boxSize){
-       g.setColor(Color.green);
-       g.fillRect(x,y,width-boxSize*4,height);
-       g.setColor(Color.lightGray);
-       g.fillRect(x+boxSize,y,boxSize,height-boxSize);
-       g.fillRect(x+boxSize,y+height-(boxSize*2),boxSize*5,boxSize);
-       g.fillRect(x+boxSize*5,y+boxSize,boxSize,height-boxSize*2);
-       g.fillRect(x+boxSize*5,y+boxSize,boxSize*5,boxSize);
-       g.fillRect(x+boxSize*9,y+boxSize,boxSize,height-boxSize);
-   }
+    //This method draws the screen where the user can choose their map.
+    public void drawChooseMapScreen(Graphics g,int width, int height){
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,width,height);
+        g.setColor(Color.green);
+        drawMap1Screen(g,boxSize*3/2,boxSize*2,boxSize*4,boxSize*3,16);
+        g.setColor(Color.cyan);
+        drawMap2Screen(g,boxSize*13/2,boxSize*2,boxSize*4,boxSize*3,16);
+        g.setColor(Color.pink);
+        drawMap3Screen(g,boxSize*23/2,boxSize*2,boxSize*4,boxSize*3,16);
+        g.setColor(Color.WHITE);
+        g.drawString("Choose Your Map:",width/2-70,50);
+        g.drawRect(boxSize,boxSize*2,boxSize*4,boxSize*3);
+        g.drawRect(boxSize*6,boxSize*2,boxSize*4,boxSize*3);
+        g.drawRect(boxSize*11,boxSize*2,boxSize*4,boxSize*3);
+    }
 
 
-   //This method draws the screen for the second map.
-   public void drawMap2Screen(Graphics g,int x, int y,int width, int height,int boxSize){
-       g.setColor(Color.cyan);
-       g.fillRect(x,y,width-boxSize*4,height);
-   }
+    //This method draws the screen for the first map.
+    public void drawMap1Screen(Graphics g,int x, int y,int width, int height,int boxSize){
+        g.setColor(Color.green);
+        g.fillRect(x,y,width-boxSize*4,height);
+        g.setColor(Color.lightGray);
+        g.fillRect(x+boxSize,y,boxSize,height-boxSize);
+        g.fillRect(x+boxSize,y+height-(boxSize*2),boxSize*5,boxSize);
+        g.fillRect(x+boxSize*5,y+boxSize,boxSize,height-boxSize*2);
+        g.fillRect(x+boxSize*5,y+boxSize,boxSize*5,boxSize);
+        g.fillRect(x+boxSize*9,y+boxSize,boxSize,height-boxSize);
+    }
 
 
-   //This method draws the screen for the third map.
-   public void drawMap3Screen(Graphics g,int x, int y,int width, int height,int boxSize){
-       g.setColor(Color.pink);
-       g.fillRect(x,y,width-boxSize*4,height);
-   }
+    //This method draws the screen for the second map.
+    public void drawMap2Screen(Graphics g,int x, int y,int width, int height,int boxSize){
+        g.setColor(Color.cyan);
+        g.fillRect(x,y,width-boxSize*4,height);
+    }
 
 
-   public void drawShopScreen(Graphics g,int width, int height){
-       width = width-256;
-       g.setColor(Color.lightGray);
-       g.fillRect(width,0,boxSize*4,height);
-       g.setColor(Color.WHITE);
-       g.fillRect(width,0,boxSize*4,boxSize);
-       g.setColor(Color.black);
-       g.drawRect(width,0,boxSize*4,boxSize);
-       g.drawString("Shop",width + boxSize*2 -15,boxSize/2);
-       g.setColor(Color.green);
-       g.fillRect(width,height-boxSize,boxSize*4,boxSize);
-       g.setColor(Color.black);
-       g.drawRect(width,height-boxSize,boxSize*4,boxSize);
-       g.drawString("Start Level",width + boxSize*2 -30,height-boxSize/2);
-       //draws 9 circles in a grid. The number of rows is the upper bound of i, and there are three per row.
-       //the number of rows can be any int between 2 and 9 for circles to appear in the proper area.
-       int spacing = 15;
-       for(int i = 1; i < 3; i++) {
-           for(int j = 0; j < 3;j++) {
-               g.drawOval(width + (j)*boxSize + (j+1)*spacing, i * (boxSize + spacing), boxSize, boxSize);
-           }
-       }
-   }
+    //This method draws the screen for the third map.
+    public void drawMap3Screen(Graphics g,int x, int y,int width, int height,int boxSize){
+        g.setColor(Color.pink);
+        g.fillRect(x,y,width-boxSize*4,height);
+    }
+
+
+    public void drawShopScreen(Graphics g,int width, int height){
+        width = width-256;
+        g.setColor(Color.lightGray);
+        g.fillRect(width,0,boxSize*4,height);
+        g.setColor(Color.WHITE);
+        g.fillRect(width,0,boxSize*4,boxSize);
+        g.setColor(Color.black);
+        g.drawRect(width,0,boxSize*4,boxSize);
+        g.drawString("Shop",width + boxSize*2 -15,boxSize/2);
+        g.setColor(Color.green);
+        g.fillRect(width,height-boxSize,boxSize*4,boxSize);
+        g.setColor(Color.black);
+        g.drawRect(width,height-boxSize,boxSize*4,boxSize);
+        g.drawString("Start Level",width + boxSize*2 -30,height-boxSize/2);
+        //draws 9 circles in a grid. The number of rows is the upper bound of i, and there are three per row.
+        //the number of rows can be any int between 2 and 9 for circles to appear in the proper area.
+        int spacing = 15;
+        for(int i = 1; i < 3; i++) {
+            for(int j = 0; j < 3;j++) {
+                g.drawOval(width + (j)*boxSize + (j+1)*spacing, i * (boxSize + spacing), boxSize, boxSize);
+            }
+        }
+    }
 
 
 }
--- TowerDefense.java
+++ TowerDefense.java
@@ -4,16 +4,16 @@ import java.awt.event.*;
 
 
 public class TowerDefense extends JPanel implements MouseListener, KeyListener {
-   //change panel width and height so that we can have a shop panel and game panel
-   //which are independent of the final width and height.
-   public static final int WIDTH = 1024;
-   public static final int HEIGHT = 768;
-   public static final int FPS = 60;
-   public static int popped = 0;
-   public static int moolah = 0;
+    //change panel width and height so that we can have a shop panel and game panel
+    //which are independent of the final width and height.
+    public static final int WIDTH = 1024;
+    public static final int HEIGHT = 768;
+    public static final int FPS = 60;
+    public static int popped = 0;
+    public static int tokens = 0;
 
-<<<<<<< HEAD
     //these booleans and buttons create a relationship between buttons and screen to determine which screens are displaying
+    Screen screen;
     boolean titleScreen;
     static Button Start = new Button(WIDTH/2-20,HEIGHT*5/8-20,50,30);
     boolean chooseMapScreen;
@@ -23,166 +23,150 @@ public class TowerDefense extends JPanel implements MouseListener, KeyListener {
     Button map2ScreenButton = new Button(Screen.boxSize*13/2,Screen.boxSize*2,Screen.boxSize*4,Screen.boxSize*3);
     boolean map3Screen;
     Button map3ScreenButton = new Button(Screen.boxSize*23/2,Screen.boxSize*2,Screen.boxSize*4, Screen.boxSize*3);
-    Screen screen;
-    static boolean titleScreen;
-    static boolean chooseMapScreen;
-    static boolean map1Screen;
-    static boolean map2Screen;
-    static boolean map3Screen;
-=======
-   Screen screen;
 
-   boolean titleScreen;
-    static Button Start = new Button(WIDTH/2-20,HEIGHT*5/8-20,50,30);
-    boolean chooseMapScreen;
-   boolean map1Screen;
-   Button map1ScreenButton = new Button(Screen.boxSize*3/2,Screen.boxSize*2,Screen.boxSize*4,Screen.boxSize*3);
-   boolean map2Screen;
-   Button map2ScreenButton = new Button(Screen.boxSize*13/2,Screen.boxSize*2,Screen.boxSize*4,Screen.boxSize*3);
-   boolean map3Screen;
-   Button map3ScreenButton = new Button(Screen.boxSize*23/2,Screen.boxSize*2,Screen.boxSize*4, Screen.boxSize*3);
+    public TowerDefense(){
+        addKeyListener(this);
+        addMouseListener(this);
 
->>>>>>> parent of e45ffb4 (Contemplating a ButtonHolder class)
-
-   public TowerDefense(){
-       addKeyListener(this);
-       addMouseListener(this);
+        titleScreen = true;
+        chooseMapScreen = false;
+        map1Screen = false;
+        map2Screen = false;
+        map3Screen = false;
+        screen = new Screen();
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        Thread mainThread = new Thread(new Runner());
+        mainThread.start();
+    }
 
 
-       titleScreen = true;
-       chooseMapScreen = false;
-       map1Screen = false;
-       map2Screen = false;
-       map3Screen = false;
-       arrayOfButtons = new ButtonHolder();
-       screen = new Screen();
-       this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-       Thread mainThread = new Thread(new Runner());
-       mainThread.start();
-   }
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Amherst Tower Defense");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        TowerDefense mainInstance = new TowerDefense();
+        frame.setContentPane(mainInstance);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 
-   public static void main(String[] args){
-       JFrame frame = new JFrame("Amherst Tower Defense");
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       TowerDefense mainInstance = new TowerDefense();
-       frame.setContentPane(mainInstance);
-       frame.pack();
-       frame.setVisible(true);
-   }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        if (titleScreen){
+            screen.drawTitleScreen(g,getWidth(),getHeight());
+        }
+        else if(chooseMapScreen){
+            screen.drawChooseMapScreen(g,getWidth(),getHeight());
+        }
+        else if(map1Screen){
+            screen.drawMap1Screen(g,0,0,getWidth(),getHeight(),Screen.boxSize);
+            screen.drawShopScreen(g,getWidth(),getHeight());
+        }
+        else if(map2Screen){
+            screen.drawMap2Screen(g,0,0,getWidth(),getHeight(),Screen.boxSize);
+            screen.drawShopScreen(g,getWidth(),getHeight());
+        }
+        else if(map3Screen){
+            screen.drawMap3Screen(g,0,0,getWidth(),getHeight(),Screen.boxSize);
+            screen.drawShopScreen(g,getWidth(),getHeight());
+        }
+    }
 
 
-   public void paintComponent(Graphics g){
-       super.paintComponent(g);
-       if (titleScreen){
-           screen.drawTitleScreen(g,getWidth(),getHeight());
-       }
-       else if(chooseMapScreen){
-           screen.drawChooseMapScreen(g,getWidth(),getHeight());
-       }
-       else if(map1Screen){
-           screen.drawMap1Screen(g,0,0,getWidth(),getHeight(),Screen.boxSize);
-           screen.drawShopScreen(g,getWidth(),getHeight());
-       }
-       else if(map2Screen){
-           screen.drawMap2Screen(g,0,0,getWidth(),getHeight(),Screen.boxSize);
-           screen.drawShopScreen(g,getWidth(),getHeight());
-       }
-       else if(map3Screen){
-           screen.drawMap3Screen(g,0,0,getWidth(),getHeight(),Screen.boxSize);
-           screen.drawShopScreen(g,getWidth(),getHeight());
-       }
-   }
-
-
-   class Runner implements Runnable{
-       public void run() {
-           while(true){
-               //sc
-               repaint();
-               try{
-                   Thread.sleep(1000/FPS);
-               }
-               catch(InterruptedException e){}
-           }
-       }
-   }
+    class Runner implements Runnable{
+        public void run() {
+            while(true){
+                //sc
+                repaint();
+                try{
+                    Thread.sleep(1000/FPS);
+                }
+                catch(InterruptedException e){}
+            }
+        }
+    }
 
 
 
 
-   @Override
-   public void mouseClicked(MouseEvent e) {
-       double x = e.getX();
-       double y = e.getY();
-       System.out.println("The mouse has been clicked at " + x + ", " + y + ".");
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
+        System.out.println("The mouse has been clicked at " + x + ", " + y + ".");
 
-<<<<<<< HEAD
-       if (titleScreen) ButtonHolder.checkIfClicked('t', x, y);
-       if (chooseMapScreen) {
-           if (map1Screen) ButtonHolder.checkIfClicked('1', x, y);
-           if (map2Screen) ButtonHolder.checkIfClicked('2', x, y);
-           if (map3Screen) ButtonHolder.checkIfClicked('3', x, y);
-=======
-       if (titleScreen) {
-           if (y > Start.top && y < Start.bottom && x > Start.left && x < Start.right) {
-               titleScreen = false;
-               chooseMapScreen = true;
-           }
->>>>>>> parent of e45ffb4 (Contemplating a ButtonHolder class)
-       }
+        if (titleScreen) {
+            if (y > Start.top && y < Start.bottom && x > Start.left && x < Start.right) {        //let's move this to a button.check and ButtonHolder thing
+                titleScreen = false;
+                chooseMapScreen = true;
+            }
+        }
 
+        if (chooseMapScreen) {
+            if ((y > map1ScreenButton.top) && (y < map1ScreenButton.bottom) && (x > map1ScreenButton.left) && (x < map1ScreenButton.right)) {
+                chooseMapScreen = false;
+                map1Screen = true;
+            }
+            if ((y > map2ScreenButton.top) && (y < map2ScreenButton.bottom) && (x > map2ScreenButton.left) && (x < map2ScreenButton.right)) {
+                chooseMapScreen = false;
+                map2Screen = true;
+            }
+            if ((y > map3ScreenButton.top) && (y < map3ScreenButton.bottom) && (x > map3ScreenButton.left) && (x < map3ScreenButton.right)) {
+                chooseMapScreen = false;
+                map3Screen = true;
+            }
+        }
 
-   }
+    }
 
-   @Override
-   public void mousePressed(MouseEvent e) {
-       double x = e.getX();
-       double y = e.getY();
+    @Override
+    public void mousePressed(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
 
-   }
+    }
 
-   @Override
-   public void mouseReleased(MouseEvent e) {
-       double x = e.getX();
-       double y = e.getY();
-   }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
+    }
 
-   @Override
-   public void mouseEntered(MouseEvent e) {
-       double x = e.getX();
-       double y = e.getY();
-
-
-   }
-
-   @Override
-   public void mouseExited(MouseEvent e) {
-       double x = e.getX();
-       double y = e.getY();
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
 
 
-   }
+    }
 
-   @Override
-   public void keyTyped(KeyEvent e) {
-       char k = e.getKeyChar();
+    @Override
+    public void mouseExited(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
+
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        char k = e.getKeyChar();
         System.out.println("The key " + k + " has been pressed.");
 
-   }
+    }
 
-   @Override
-   public void keyPressed(KeyEvent e) {
-       char k = e.getKeyChar();
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char k = e.getKeyChar();
 
-   }
+    }
 
 
-   @Override
-   public void keyReleased(KeyEvent e) {
-       char k = e.getKeyChar();
+    @Override
+    public void keyReleased(KeyEvent e) {
+        char k = e.getKeyChar();
 
-   }
+    }
 
 
 }
@@ -192,56 +176,60 @@ public class TowerDefense extends JPanel implements MouseListener, KeyListener {
 
 
 abstract class Tower{
-   double x;
-   double y;
-   double damage;
-   //determine perspective, and use these if the base of the tower is different than where the bullets will be coming from.
-   double fireX;
-   double fireY;
-   int type;
-
-   //implement a purchase/upgrade button for each tower?
-
-   public void fireBullet(double fireX, double fireY, int damage) {
-       //add to an arrayList of bullets?
-   }
+    double x;
+    double y;
+    double damage;
+    //determine perspective, and use these if the base of the tower is different than where the bullets will be coming from.
+    double fireX;
+    double fireY;
+    int type;
 
 
-   public void drawTower(int type, Graphics g) {
-       g.setColor(Color.blue);
-       g.fillRect((int)x, (int)y, 50, 50);
-       if (type == 1) BasicTower.drawBasicTower();
-       //I'm not sure if x, y will work how I'm thinking they'll work ngl
-   }
+    //implement a purchase/upgrade button for each tower?
+
+
+    public void fireBullet(double fireX, double fireY, int damage) {
+        //add to an arrayList of bullets?
+    }
+
+
+    public void drawTower(int type, Graphics g) {
+        g.setColor(Color.blue);
+        g.fillRect((int)x, (int)y, 50, 50);
+        if (type == 1) BasicTower.drawBasicTower();
+        //I'm not sure if x, y will work how I'm thinking they'll work ngl
+    }
 }
 
 
 class BasicTower extends Tower {
 
 
-   public BasicTower(double x, double y) {
-       this. x = x;
-       this.y = y;
-       damage = 1;
-       type = 1;
-   }
 
 
-   @Override
-   public void fireBullet(double fireX, double fireY, int damage) {
-       super.fireBullet(fireX, fireY, (int) this.damage);
-   }
+    public BasicTower(double x, double y) {
+        this. x = x;
+        this.y = y;
+        damage = 1;
+        type = 1;
+    }
 
 
-   @Override
-   public void drawTower(int type, Graphics g) {
-       super.drawTower(this.type, g);
-   }
+    @Override
+    public void fireBullet(double fireX, double fireY, int damage) {
+        super.fireBullet(fireX, fireY, (int) this.damage);
+    }
 
 
-   public static void drawBasicTower() {
-       //here we can add the "basic" graphics to the tower
-   }
+    @Override
+    public void drawTower(int type, Graphics g) {
+        super.drawTower(this.type, g);
+    }
+
+
+    public static void drawBasicTower() {
+        //here we can add the "basic" graphics to the tower
+    }
 }
 
 
@@ -250,27 +238,27 @@ class Balloons{
 
 
 class Pair{
-   public double x;
-   public double y;
-   public Pair(double initX, double initY){
-       x = initX;
-       y = initY;
-   }
+    public double x;
+    public double y;
+    public Pair(double initX, double initY){
+        x = initX;
+        y = initY;
+    }
 
 
-   public Pair add(Pair toAdd){
-       return new Pair(x + toAdd.x, y + toAdd.y);
-   }
+    public Pair add(Pair toAdd){
+        return new Pair(x + toAdd.x, y + toAdd.y);
+    }
 
 
-   public Pair divide(double denominator){
-       return new Pair(x / denominator, y / denominator);
-   }
+    public Pair divide(double denominator){
+        return new Pair(x / denominator, y / denominator);
+    }
 
 
-   public Pair times(double val){
-       return new Pair(x * val, y * val);
-   }
+    public Pair times(double val){
+        return new Pair(x * val, y * val);
+    }
 }
 
 
@@ -279,13 +267,13 @@ class Button {
     double y;
     double width;
     double height;
-    //The coordinate for the upper side corner of the button
+    //The (y) coordinate for the upper side corner of the button
     double top;
-    //The coordinate for the lower side of the button
+    //The (y) coordinate for the lower side of the button
     double bottom;
-    //The coordinate for the left side corner of the button
+    //The (x) coordinate for the left side corner of the button
     double left;
-    //The coordinate for the right side of the button
+    //The (x) coordinate for the right side of the button
     double right;
 
     public Button(double x, double y, double width, double height) {
@@ -305,62 +293,8 @@ class Button {
 
 }
 
-<<<<<<< HEAD
 class ButtonHolder {
-    double width;
-    double height;
-    public static Button Start;
 
 
-    public ButtonHolder() {
-        width = TowerDefense.WIDTH;
-        height = TowerDefense.HEIGHT;
-        Start = new Button(width/2-20,height*5/8-20,50,30);         //this is the title screen "Start" button
-        Button[] arrayOfButtons = new Button[5];
-        arrayOfButtons[0] = Start;
-        arrayOfButtons[1] = new Button(Screen.boxSize*3/2,Screen.boxSize*2,Screen.boxSize*4,Screen.boxSize*3);      //this is the map1ScreenButton;
-        arrayOfButtons[2] = new Button(Screen.boxSize*13/2,Screen.boxSize*2,Screen.boxSize*4,Screen.boxSize*3);     //this is the map2ScreenButton;
-        arrayOfButtons[3] = new Button(Screen.boxSize*23/2,Screen.boxSize*2,Screen.boxSize*4, Screen.boxSize*3);    //this is the map3ScreenButton;
+}
 
-
-    }
-
-    public static boolean clickRange(Button button) {
-        double y = button.y;
-        double x = button.x;
-        if (y > button.top && y < button.bottom && x > button.left && x < button.right) {
-            return true;
-        }
-        else return false;
-    }
-
-    public static void checkIfClicked(char i, double x, double y) {
-        Button CB = TowerDefense.arrayOfButtons[i];      //CB = current button
-        if (i == 't') {
-                if (clickRange(CB)) {
-                    TowerDefense.titleScreen = false;
-                    TowerDefense.chooseMapScreen = true;
-                }
-        }
-//        if (i == '1') {
-//            if ((y > CB.top) && (y < CB.bottom) && (x > CB.left) && (x < CB.right)) {
-//                chooseMapScreen = false;
-//                map1Screen = true;
-//            }
-//        }
-//        if (i == '2') {
-//            if ((y > map2ScreenButton.top) && (y < map2ScreenButton.bottom) && (x > map2ScreenButton.left) && (x < map2ScreenButton.right)) {
-//                chooseMapScreen = false;
-//                map2Screen = true;
-//            }
-//        }
-//        if (i == '3') {
-//            if ((y > map3ScreenButton.top) && (y < map3ScreenButton.bottom) && (x > map3ScreenButton.left) && (x < map3ScreenButton.right)) {
-//                chooseMapScreen = false;
-//                map3Screen = true;
-//            }
-        }
-
-    }
-=======
->>>>>>> parent of e45ffb4 (Contemplating a ButtonHolder class)
