2df44a9745332c716adea167cc0ee43e2107b0a7
hen-er <136639931+hen-er@users.noreply.github.com>
2023-12-04 15:01:08 -0500

    beck did this ~
    troubleshooting

--- Bullet.java
+++ Bullet.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Bullet {
     //I'm thinking we might also need a boolean for "has target" or a boolean for ballons called "is targetted"
     //prolly determined by position of tower
@@ -15,4 +17,14 @@ public class Bullet {
     public Pair getPosition(){return position;}
     public int getDamage() {return damage;}
     public Pair getVelocity() {return velocity;}
+    public void setVelocity(Pair target){
+        //using this to determine the initial firing direction of the balloon
+        //this is also what we're gonna use if we make "tracking balloons"
+        this.velocity.x = (target.x-position.x)/0.3;
+        this.velocity.y = (target.y-position.y)/0.3;
+    }
+    public void drawComponent(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillOval((int)(position.x - radius), (int)(position.y - radius), (int)(2*radius), (int)(2*radius));
+    }
 }
